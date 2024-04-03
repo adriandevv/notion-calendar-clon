2309efcf669952900b48a737418a5f799a36b851
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-04-02 19:42:14 -0600

    Fix layout and add client-side code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+"use client";
 import { Calendar } from "@/components/Calendar";
 import { ToolsHeader } from "@/components/ToolsHeader";
-
+import React from "react";
+import { useGetMonth } from "./hooks/useGetMonth";
 export default function Home() {
+  const { monthIndex, setMonthIndex, currentMonth, isCurrentMonth } = useGetMonth();
+
   return (
     <main>
-      <ToolsHeader />
-      <Calendar  />
+      <ToolsHeader setMonthIndex={setMonthIndex} monthIndex={monthIndex}  />
+      <Calendar  currentMonth={currentMonth} isCurrentMonth={isCurrentMonth}/>
     </main>
   );
 }

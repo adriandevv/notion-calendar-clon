f81202d22483b9d6ecd82f8c4c7537ad7cce39dc
Adrian Valencia Hernandez <adrian15996@gmail.com>
2024-04-06 13:16:22 -0600

    Update layout and add SmallCalendar and SideBar components

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,22 @@ import { Calendar } from "@/components/Calendar";
 import { ToolsHeader } from "@/components/ToolsHeader";
 import React from "react";
 import { useGetMonth } from "./hooks/useGetMonth";
+import SideBar from "@/components/SideBar";
+import { SmallCalendar } from "@/components/SmallCalendar";
 export default function Home() {
-  const { monthIndex, setMonthIndex, currentMonth, isCurrentMonth } = useGetMonth();
+  const { monthIndex, setMonthIndex, currentMonth, isCurrentMonth } =
+    useGetMonth();
 
-  return (
-    <main>
-      <ToolsHeader setMonthIndex={setMonthIndex} monthIndex={monthIndex}  />
-      <Calendar  currentMonth={currentMonth} isCurrentMonth={isCurrentMonth}/>
-    </main>
-  );
-}
+    return (
+      <main className="min-h-screen min-w-screen flex">
+        <section className="w-96 my-28">
+        <SmallCalendar />
+        </section>
+        <section className="flex flex-col w-full relative ">
+          <ToolsHeader setMonthIndex={setMonthIndex} monthIndex={monthIndex} />
+          <Calendar   currentMonth={currentMonth} isCurrentMonth={isCurrentMonth} />
+        </section>
+        <SideBar />
+      </main>
+    );
+  }
